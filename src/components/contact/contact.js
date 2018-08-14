f726d1d775a8f6a6ada1e881c3a89697334be762
Noah Strayve <noahstrayve@gmail.com>
2018-08-14 13:37:54 -0500

    initialized page directories

---
+++ src/components/contact/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Contact.css';
+import { Link } from "react-router-dom";
+
+const ContactPage = props => {
+	return <div className="contactPage">
+
+			</div>;
+}
+
+export default ContactPage;
