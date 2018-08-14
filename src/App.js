63e7db79a704052d271e4bfa6864c516c3c2f92c
Noah Strayve <noahstrayve@gmail.com>
2018-08-14 13:09:00 -0500

    started home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Headline from './components/header/header';
+import HomePage from './components/landing/landing';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <Headline/>
+    	<div>
+      		<Headline/>
+      		<HomePage/>
+      	</div>
     );
   }
 }
--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -3,8 +3,62 @@ import './Landing.css';
 import { Link } from "react-router-dom";
 
 const HomePage = props => {
-	return <div className="landingMainImage">
-				<p>AYYYY RENDERING IS WORKING</p>
+	return <div className="landingPage">
+				<div className="landingMainImage">
+					<div className="welcomeMessageBox">
+						<div className="welcomeMessageHeadline">
+							<h3>PLZ HALP</h3>
+						</div>
+						<div className="welcomeMessageSubtext">
+							<p>We need money to help poor people</p>
+						</div>
+					</div>
+				</div>
+				<div className="statsContainer">
+					<div className="statsTitle">
+						<h1>STATISTICS</h1>
+						<hr className="statTitleBreak"/>
+					</div>
+					<div className="statLayer">
+						<div className="statSideOne">
+							<h1>25</h1>
+						</div>
+						<div className="statSideTwo">
+							<div className="statHeadline">
+								<h2>Alarming Text</h2>
+							</div>
+							<div className="statSubtext">
+								<p>Shoopity Poop Scoopty Poopty Scoopdy Whoop</p>
+							</div>
+						</div>
+					</div>
+					<div className="statLayer">
+						<div className="statSideOne">
+							<div className="statHeadline">
+								<h2>Alarming Text</h2>
+							</div>
+							<div className="statSubtext">
+								<p>Shoopity Poop Scoopty Poopty Scoopdy Whoop</p>
+							</div>
+						</div>
+						<div className="statSideTwo">
+							<h1>25</h1>
+						</div>
+					</div>
+					<div className="statLayer">
+						<div className="statSideOne">
+							<h1>25</h1>
+						</div>
+						<div className="statSideTwo">
+							<div className="statHeadline">
+								<h2>Alarming Text</h2>
+							</div>
+							<div className="statSubtext">
+								<p>Shoopity Poop Scoopty Poopty Scoopdy Whoop</p>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>;
 }
 
