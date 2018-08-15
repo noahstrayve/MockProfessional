5efdc075d4237cc2ee8261314b9bacf8dfd5731b
Noah Strayve <noahstrayve@gmail.com>
2018-08-15 14:02:29 -0500

    resized image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Headline from './components/header/header';
 import HomePage from './components/landing/landing';
+import TeamPage from './components/team/team';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
@@ -34,6 +35,7 @@ class App extends Component {
                 <Route exact path="/team" 
                    render={() => <div>
                                     <Headline />
+                                    <TeamPage />
                                   </div>} />
                                   
       </div>
--- src/components/team/team.js
+++ src/components/team/team.js
@@ -4,7 +4,35 @@ import { Link } from "react-router-dom";
 
 const TeamPage = props => {
 	return <div className="teamPage">
+				<div className="teamTitleBox">
 
+				</div>
+				<div className="teamMemberLayer">
+					<div className="teamMemberCard">
+						<div className="teamMemberImageHolder">
+							
+						</div>
+					</div>
+					<div className="teamMemberCard">
+
+					</div>
+				</div>
+				<div className="teamMemberLayer">
+					<div className="teamMemberCard">
+
+					</div>
+					<div className="teamMemberCard">
+
+					</div>
+				</div>
+				<div className="teamMemberLayer">
+					<div className="teamMemberCard">
+
+					</div>
+					<div className="teamMemberCard">
+
+					</div>
+				</div>
 			</div>;
 }
 
